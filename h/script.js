de23774593fe8a5af6b1f77b035813d076f882ca
vivek-rn <43517224+vivek-rn@users.noreply.github.com>
2023-10-12 00:59:16 +0530

    Add files via upload

---
+++ h/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    var myAudio = new Audio();
+        myAudio.src = "/happy.mp3";
+    myAudio.play();
+});
+let confetti = new Confetti('confetti');
+
+confetti.destroyTarget(false);
+
+confetti.setCount(1000);
+confetti.setSize(1);
+confetti.setPower(60);
+confetti.setFade(false);
+confetti.destroyTarget(false);
+
+window.addEventListener("load", (event) => {
+    document.getElementById("confetti").click();
+    document.getElementById("confetti").click();
+});
+
+$(".full-landing-image").ripples({
+    resolution: 256,
+    perturbance: 0.02,
+  });
